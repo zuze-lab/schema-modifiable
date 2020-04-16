dea5cfa40cf43d7c74ccc5fd93ca91a4c5783254
akmjenkins <akmjenkins@gmail.com>
2020-04-16 12:40:12 -0300

    Remove babel dependency and upgrade zuzze packages

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,40 @@
 import { from, get, set } from '@zuze/interpolate';
 
-export const allKeys = (...objects) =>
-  Array.from(
-    new Set(objects.reduce((acc, o) => acc.concat(Object.keys(o)), []))
+const fromArgs = args => Array.prototype.slice.call(args);
+const fromSet = set => {
+  const r = [];
+  return set.forEach(v => r.push(v)), r;
+};
+
+export function allKeys() {
+  return fromSet(
+    new Set(
+      fromArgs(arguments).reduce((acc, o) => acc.concat(Object.keys(o)), [])
+    )
   );
+}
 
 export const deep = (state, mod) =>
   Object.entries(mod).reduce(
-    (acc, [key, val]) => set(acc, key, val, true),
+    (acc, entry) => set(acc, entry[0], entry[1], true),
     state
   );
 
 // accepts paths and returns a function that merges the values of objects at those paths
-export const merge = keys => (...objects) =>
-  keys.reduce(
-    (acc, k) =>
-      set(
-        acc,
-        k,
-        objects.reduce((acc, o) => ({ ...acc, ...get(o, k, {}) }), {})
-      ),
-    {}
-  );
+
+export const merge = keys =>
+  function() {
+    const objects = fromArgs(arguments);
+    return keys.reduce(
+      (acc, k) =>
+        set(
+          acc,
+          k,
+          objects.reduce((acc, o) => Object.assign(acc, get(o, k, {})), {})
+        ),
+      {}
+    );
+  };
 
 // keys reference expected array entities
 export const append = keys => (orig, next) =>
@@ -45,9 +58,12 @@ export const complexMerge = ({
       const val = get(acc, k);
       return val ? set(acc, k, from(val, context)) : acc;
     },
-    keeps.reduce((acc, k) => set(acc, k, get(state, k)), {
-      ...deep(state, mod),
-      ...merge(merges)(state, mod),
-      ...append(appends)(state, mod),
-    })
+    keeps.reduce(
+      (acc, k) => set(acc, k, get(state, k)),
+      Object.assign(
+        deep(state, mod),
+        merge(merges)(state, mod),
+        append(appends)(state, mod)
+      )
+    )
   );
